74b3066698e78c71c61d369234fe306d5b0303bb
Daria0304-gr <72433055+Daria0304-gr@users.noreply.github.com>
2020-10-17 21:57:13 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using InspectorLib;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inspect funk = new Inspect();
+            Console.WriteLine($"{funk.GetInspector()}");
+            Console.WriteLine($"{funk.GenerateNumber("456", "d".ToUpper(), "75")}");
+            Console.WriteLine($"{funk.GetWorker()}");
+            Console.WriteLine($"{funk.AddWdorker()}");
+
+            Console.ReadLine();
+        }
+    }
+}
